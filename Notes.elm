c7e82e4b59ae5e439c55ce4c280805c06ccbcf35
Peter Jang <peter.x.jang@gmail.com>
2017-09-25 10:58:24 -0500

    Convert HTML template to Elm

---
+++ Notes.elm
@@ -0,0 +1,34 @@
+module Notes exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+main =
+    div [ id "app" ]
+        [ div [ class "toolbar" ]
+            [ button [ class "toolbar-button" ] [ text "New" ]
+            , button [ class "toolbar-button" ] [ text "Delete" ]
+            , input [ class "toolbar-search", type_ "text", placeholder "Search..." ] []
+            ]
+        , div [ class "note-container" ]
+            [ div [ class "note-selectors" ]
+                [ div [ class "note-selector active" ]
+                    [ p [ class "note-selector-title" ] [ text "First note..." ]
+                    , p [ class "note-selector-timestamp" ] [ text "Timestamp here..." ]
+                    ]
+                , div [ class "note-selector" ]
+                    [ p [ class "note-selector-title" ] [ text "Second note..." ]
+                    , p [ class "note-selector-timestamp" ] [ text "Timestamp here..." ]
+                    ]
+                , div [ class "note-selector" ]
+                    [ p [ class "note-selector-title" ] [ text "Third note..." ]
+                    , p [ class "note-selector-timestamp" ] [ text "Timestamp here..." ]
+                    ]
+                ]
+            , div [ class "note-editor" ]
+                [ p [ class "note-editor-info" ] [ text "Timestamp here..." ]
+                , textarea [ class "note-editor-input" ] [ text "First note..." ]
+                ]
+            ]
+        ]
