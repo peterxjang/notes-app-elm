0fd3fd2c0d66c5c37370742bcd6e4780f55e34b0
Peter Jang <peter.x.jang@gmail.com>
2018-08-27 16:47:49 -0500

    Display note titles from an array of notes

--- Notes.elm
+++ Notes.elm
@@ -1,10 +1,71 @@
 module Notes exposing (main)
 
+import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
 main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- MODEL
+
+
+type alias Note =
+    { id : Int, body : String, timestamp : Int }
+
+
+type alias Model =
+    { notes : List Note }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { notes =
+            [ { id = 1, body = "First note...", timestamp = 0 }
+            , { id = 2, body = "Second note...", timestamp = 0 }
+            , { id = 3, body = "Third note...", timestamp = 0 }
+            ]
+      }
+    , Cmd.none
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
     div [ id "app" ]
         [ div [ class "toolbar" ]
             [ button [ class "toolbar-button" ] [ text "New" ]
@@ -12,23 +73,24 @@ main =
             , input [ class "toolbar-search", type_ "text", placeholder "Search..." ] []
             ]
         , div [ class "note-container" ]
-            [ div [ class "note-selectors" ]
-                [ div [ class "note-selector active" ]
-                    [ p [ class "note-selector-title" ] [ text "First note..." ]
-                    , p [ class "note-selector-timestamp" ] [ text "Timestamp here..." ]
-                    ]
-                , div [ class "note-selector" ]
-                    [ p [ class "note-selector-title" ] [ text "Second note..." ]
-                    , p [ class "note-selector-timestamp" ] [ text "Timestamp here..." ]
-                    ]
-                , div [ class "note-selector" ]
-                    [ p [ class "note-selector-title" ] [ text "Third note..." ]
-                    , p [ class "note-selector-timestamp" ] [ text "Timestamp here..." ]
-                    ]
-                ]
+            [ viewNoteSelectors model
             , div [ class "note-editor" ]
                 [ p [ class "note-editor-info" ] [ text "Timestamp here..." ]
                 , textarea [ class "note-editor-input" ] [ text "First note..." ]
                 ]
             ]
+        ]
+
+
+viewNoteSelectors : Model -> Html Msg
+viewNoteSelectors model =
+    div [ class "note-selectors" ]
+        (List.map (\note -> viewNoteSelector note) model.notes)
+
+
+viewNoteSelector : Note -> Html Msg
+viewNoteSelector note =
+    div [ class "note-selector" ]
+        [ p [ class "note-selector-title" ] [ text note.body ]
+        , p [ class "note-selector-timestamp" ] [ text (String.fromInt note.timestamp) ]
         ]
